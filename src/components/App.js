cc804b311edfe6b085bacf847183713c6a713eac
somratcste <somratcste@gmail.com>
2021-02-06 11:44:10 +0600

    refactor: use state

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react'
 import './App.css';
 import First from "./First/First";
-import Counter from "./Counter/Counter";
 
 class App extends Component {
     state = {
@@ -13,10 +12,9 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                {/*{this.state.persons.map( (person, index) => {*/}
-                {/*    return <First key={index} name={person.name} />*/}
-                {/*} )}*/}
-                <Counter/>
+                {this.state.persons.map( (person, index) => {
+                    return <First key={index} name={person.name} />
+                } )}
             </div>
         );
     }
