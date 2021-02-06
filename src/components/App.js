321c1b26907dd3ee3b8d29651bb149c59292eb0a
somratcste <somratcste@gmail.com>
2021-02-06 10:45:24 +0600

    feat: try react component

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import First from "./First/First";
+
+function App() {
+  return (
+    <div className="App">
+        <h1>Welcome to React Application</h1>
+        <First />
+        <First />
+        <First />
+    </div>
+  );
+}
+
+export default App;
