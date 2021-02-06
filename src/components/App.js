393dc214bb78c750af211e06cecb5f27912e87d2
somratcste <somratcste@gmail.com>
2021-02-06 11:19:42 +0600

    feat: react state

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,23 @@
+import React, { Component } from 'react'
 import './App.css';
 import First from "./First/First";
 
-function App() {
-  return (
-    <div className="App">
-        <h1>Welcome to React Application</h1>
-        <First name='nazmul' />
-        <First name='hossain' />
-    </div>
-  );
+class App extends Component {
+    state = {
+        persons: [
+            {name: 'nazmul'},
+            {name: 'hossain'}
+        ]
+    }
+    render() {
+        return (
+            <div className="App">
+                {this.state.persons.map( (person, index) => {
+                    return <First key={index} name={person.name} />
+                } )}
+            </div>
+        );
+    }
 }
 
 export default App;
